13e5d67d4802a226c3c15b6bce146bbdf5db2bf8
Faith_Oriest <1989764384@qq.com>
2026-07-25 15:41:12 +0800

    implement sysinfo system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -658,3 +658,22 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64
+nproc(void)
+{
+  struct proc *p;
+  uint64 count = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++){
+    acquire(&p->lock);
+
+    if(p->state != UNUSED){
+      count++;
+    }
+
+    release(&p->lock);
+  }
+
+  return count;
+}
