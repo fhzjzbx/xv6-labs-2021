609f3f372f0edb10daf1c52299f3feb21bd26f60
Faith_Oriest <1989764384@qq.com>
2026-08-04 10:27:59 +0800

    complete multithreading lab

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,54 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+
+  int current_round;
+  /*
+   * bstate.nthread 和 bstate.round 是多个线程共享的数据
+   * 因此访问和修改它们之前必须获得互斥锁
+   */
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  /*
+   * 保存当前线程进入屏障时所处的轮次
+   * 等待线程将通过该值判断本轮是否已经结束
+   */
+  current_round = bstate.round;
+
+  /*
+   * 当前线程已经到达本轮屏障。
+   */
+  bstate.nthread++;
+
+  /*
+   * 如果当前线程是本轮最后一个到达屏障的线程，
+   * 则由它负责结束本轮，推进到下一轮
+   */
+  if(bstate.nthread == nthread){
+    /*
+     * 为下一轮重新统计到达线程数，同时推进barrier
+     */
+    bstate.nthread = 0;
+    bstate.round++;
+    /*
+     * 唤醒所有正在等待本轮完成的线程。
+     */
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }else{
+    /*
+     * 当前线程不是最后到达的，需要等待
+     * 使用 while 可以处理虚假唤醒，并在醒来后重新检查条件
+     */
+    while(current_round == bstate.round){
+      pthread_cond_wait(&bstate.barrier_cond,
+                        &bstate.barrier_mutex);
+    }
+  }
+
+  /*
+   * 当前线程已经可以通过屏障，释放互斥锁。
+   */
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
